c57512bfc3a23586d4bb5afdac94a019758f1372
Rasmus Andersen <recan@elektro.dtu.com>
2020-09-25 10:06:24 +0200

    added functionality to specify the topics to use for semantic

--- semantics.py
+++ semantics.py
@@ -18,6 +18,7 @@ class Semantic(object):
                 except KeyError:
                     # bibentry doesn't have mendeley-tags
                     pass
+        self.list_of_tags = [x.lower() for x in self.list_of_tags]
 
 
 class SemanticGraph(pgv.AGraph):
@@ -25,7 +26,7 @@ class SemanticGraph(pgv.AGraph):
         # colors
         self.my_colorscheme = 'X11'
         self.paper_fill_color = 'deepskyblue2'
-        self.tag_fill_color = 'brown2'
+        self.tag_fill_color = '#ebe134'
         self.node_outline_color = 'navy'
         self.edge_color = 'black'
         self.node_font_color = 'navy'
@@ -40,10 +41,14 @@ class SemanticGraph(pgv.AGraph):
         self.tag_ids = {}
 
     def add_tags_nodes(self, semantics):
+        import random
+        random.seed(1234)
+        r = lambda: random.randint(100, 255)
+
         for idx, tag in enumerate(semantics.list_of_tags):
             graph_label = '{tag}&#92;n'.format(tag=tag)
             self.tag_ids[tag] = idx
-            self.add_node(idx, label=graph_label, style='filled', color=self.node_outline_color, penwidth=3, fillcolor=self.tag_fill_color, fontcolor=self.node_font_color)
+            self.add_node(idx, label=graph_label, style='filled', color=self.node_outline_color, penwidth=3, fillcolor='#%02X%02X%02X' % (r(), r(), r()), fontcolor=self.node_font_color)
             #self.node_attr.update(style='filled', color=self.node_outline_color, penwidth=3, fillcolor=self.tag_fill_color, fontcolor=self.node_font_color)
 
     def add_paper_nodes(self, semantics):
@@ -56,11 +61,18 @@ class SemanticGraph(pgv.AGraph):
     def add_paper_edges(self, semantics):
         for paper_id, paper in enumerate(semantics.bibsource.entries, start=len(semantics.list_of_tags)):
             try:
+                has_tag = False
                 for paper_tag in paper['mendeley-tags'].split(','):
-                    if paper_tag in semantics.list_of_tags:
-                        self.add_edge(paper_id, self.tag_ids[paper_tag], label='', weigth=2, dir='none', penwidth=1, color=self.edge_color)
+                    if paper_tag.lower() in semantics.list_of_tags:
+                        self.add_edge(paper_id, self.tag_ids[paper_tag.lower()], label='', weigth=2, dir='none', penwidth=1, color=self.edge_color)
+                        has_tag = True
+                if not has_tag:
+                    print('\033[93mWarning:\033[0m Removing node from graph --> no tags-connection: \033[94m{}\033[0m'.format(paper['title']))
+                    self.remove_node(self.get_node(paper_id))
+
             except KeyError:
                 # paper doesn't have mendeley-tags, so flag remove color to indicate it will not have any connections
-                n = self.get_node(paper_id)
-                n.attr['fillcolor'] = 'white'
-                pass
+                print('\033[93mWarning:\033[0m Removing node from graph --> no mendeley-tags: \033[94m{}\033[0m'.format(paper['title']))
+                self.remove_node(self.get_node(paper_id))
+
+                #n.attr['fillcolor'] = 'white'
